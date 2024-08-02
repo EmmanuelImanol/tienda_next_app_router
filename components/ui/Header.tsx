342eaa85ba49192ecabe16b0cd16e1092ad8a0b9
Imanol <emanuelimanol.vaca@gmail.com>
2024-08-02 16:44:58 -0600

    fix

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -5,9 +5,12 @@ export default function Header() {
     return (
         <div className="bg-gradient-to-r from-violet-600 to-purple-700 shadow-md w-full fixed z-10">
             <div className="max-w-screen-xl w-[95%] m-auto flex justify-between items-center h-20">
-                <h1 className="font-black text-3xl text-purple-100">Logo</h1>
                 <Link
-                    className=" text-purple-900 p-2 bg-purple-100 font-bold rounded-full hover:bg-purple-600 transition-colors hover:text-purple-100"
+                    href={'/'}
+                    className="font-black text-3xl text-purple-100"
+                >App<span className="text-purple-950">Tienda</span> </Link>
+                <Link
+                    className="text-purple-900 p-2 bg-purple-100 font-bold rounded-full hover:bg-purple-600 transition-colors hover:text-purple-100"
                     href={'/carrito'}
                 > <ShoppingCartIcon className='w-7 h-7' /> </Link>
             </div>
